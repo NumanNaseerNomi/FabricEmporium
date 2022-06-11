0681ec697763c6419a2b27ef177c44d8327d6b44
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-06-12 03:11:32 +0500

    Initial commit

---
+++ resources/views/Components/header.blade.php
@@ -0,0 +1,201 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fabric Emporieum Online</title>
+	<link href="{{ asset('plugins/bootstrap/bootstrap.min.css') }}" rel="stylesheet" />
+	<!-- <script src="{{asset('js/app.js') }}" defer></script> -->
+	<link href="{{ asset('css/style.css') }}" rel = "stylesheet" />
+	<link href="{{ asset('plugins/fontAwesome/css/fontAwesomeAll.min.css') }}" rel = "stylesheet">
+
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<!-- <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/font-awesome@4.7.0/css/font-awesome.css"></script>
+
+
+	<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script> -->
+
+	<script src="js/jquery-3.5.1.min.js"></script>
+
+	<link rel="stylesheet" type="text/css" href="assets/fontAwesome/css/fontAwesomeAll.min.css">
+
+
+
+
+</head>
+<body>
+<div class="container-fluid bg-clr fw-normal fs-6 text-white">
+	<div class="d-flex justify-content-center text-white">Shop our Latest Collection of Unstitched Fabrics, Ready to Wear and more.</div>
+</div>
+
+<nav class="navbar navbar-expand-lg navbar-light bg-white mb-3 border-bottom">
+  <div class="container">
+    <a class="navbar-brand" href="/"><img class="logo" src="{{asset('images/1.png')}}"></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+   
+
+    <div class="collapse navbar-collapse " id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll " style="--bs-scroll-height: 100px;">
+        
+        
+         <form class=" d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search here...." aria-label="Search">
+        <i class="fa fa-search srch" style="font-size:24px"></i>
+      </form>
+      </ul>
+      <ul class="icons">
+
+      	<li class="me-5 mt-4"><a class="fa-icon" href=""><i class="fal fa-user"></i></a></li>
+
+      	<li class="me-5 mt-4"><a class="fa-icon" href=""><i class="fal fa-shopping-bag"></i></a></li>
+      	<li class="me-5 mt-4"><a class="fa-icon" href=""><i class="fal fa-heart"></i></a></li>
+      </ul>
+
+
+      
+    </div>
+  </div>
+</nav>
+
+<div class="container">
+<div class="row">
+	<div class="bar-links">
+		<ul class="bar-link-list d-flex justify-content-center  fs-5">
+			<li class="list-item mx-3">
+				<div class="dropdown">
+				  <a href="" class=" drop dropdown-toggle" type="button">
+				    Summer Collection
+				  </a>
+				  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+				    <li><a href="#" class="dropdown-item" type="button">
+				    Stiched
+				</a></li>
+				    <li><button class="dropdown-item" type="button">Un-Stiched</button></li>   
+				  </ul>
+				</div>
+			</li>
+			<li class="list-item mx-3">
+				<div class="dropdown">
+				  <a href="" class=" drop dropdown-toggle" type="button">
+				    Winter Collection
+				  </a>
+				  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+				    <li><button class="dropdown-item" type="button">
+
+				    Stiched
+
+				</button></li>
+				    <li><button class="dropdown-item" type="button">Un-Stiched</button></li>
+				  </ul>
+				</div>
+
+			</li>
+			<li class="list-item mx-3">
+				<div class="dropdown">
+				  <a href="" class=" drop dropdown-toggle" type="button">
+				    Ladies
+				  </a>
+				  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+				    <li><button class="dropdown-item" type="button">
+
+				    Stiched
+
+				</button></li>
+				    <li><button class="dropdown-item" type="button">Un-Stiched</button></li>
+				  </ul>
+				</div>
+
+			</li>
+			<li class="list-item mx-3">
+				<div class="dropdown">
+				  <a href="" class=" drop dropdown-toggle" type="button">
+				    Gents
+				  </a>
+				  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+				    <li><button class="dropdown-item" type="button">
+
+				    Stiched
+
+				</button></li>
+				    <li><button class="dropdown-item" type="button">Un-Stiched</button></li>
+				  </ul>
+				</div>
+
+			</li>
+			<li class="list-item mx-3">
+				<div class="dropdown">
+				  <a href="" class=" drop dropdown-toggle" type="button">
+				    Kids
+				  </a>
+				  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+				    <li><button class="dropdown-item" type="button">
+
+				    Stiched
+
+				</button></li>
+				    <li><button class="dropdown-item" type="button">Un-Stiched</button></li>
+				  </ul>
+				</div>
+
+			</li>
+			<li class="list-item mx-3">
+				<div class="dropdown">
+				  <a href="" class=" drop dropdown-toggle" type="button">
+				    Bride
+				  </a>
+				  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+				    <li><button class="dropdown-item" type="button">
+
+				    Stiched
+
+				</button></li>
+				    <li><button class="dropdown-item" type="button">Un-Stiched</button></li>
+				  </ul>
+				</div>
+
+			</li>
+			<li class="list-item mx-3">
+				<div class="dropdown">
+				  <a href="" class=" drop dropdown-toggle" type="button">
+				    Groom
+				  </a>
+				  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+				    <li><button class="dropdown-item" type="button">
+
+				    Stiched
+
+				</button></li>
+				    <li><button class="dropdown-item" type="button">Un-Stiched</button></li>
+				  </ul>
+				</div>
+				
+
+			</li>
+			<li class="list-item mx-3">
+				<div class="dropdown">
+				  <a href="" class=" drop dropdown-toggle" type="button">
+				    Party Wear
+				  </a>
+				  <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+				    <li><button class="dropdown-item" type="button">
+
+				    Stiched
+
+				</button></li>
+				    <li><button class="dropdown-item" type="button">Un-Stiched</button></li>
+				  </ul>
+				</div>
+
+
+			</li>
+		</ul>
+	</div>
+</div>
+</div>
+
+
+
+</body>
+</html>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,238 @@
+
+@include('Components.header')
+
+<div class="container-fluid">
+		<div class="row mt-4 mb-4">
+			
+
+			<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <a href=""><img src="{{asset('images/s8.jpg')}}"class="d-block w-100" alt="..."></a>
+    </div>
+    <div class="carousel-item">
+      <a href=""><img src="{{asset('images/s8.jpg')}}"class="d-block w-100" alt="..."></a>
+    </div>
+    <div class="carousel-item">
+      <a href=""><img src="{{asset('images/s7.jpg')}}"class="d-block w-100" alt="..."></a>
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+
+		</div>
+<div class="container">
+	<div class="row mb-4">
+		<div class="d-flex heading justify-content-center">
+			<h2 class="fw-bold">Summer</h2>
+		</div>
+	</div>
+<div class="row mb-4">
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12 mb-4">
+		<a href=""><img src="{{asset('images/pic1.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic2.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic3.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic4.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+</div>
+
+<div class="row mb-5">
+	<div class="d-flex justify-content-center"><a class="btn bg-clr rounded-pill px-4 py-2 fs-6" href="">View More</a></div>
+</div>
+
+<div class="row mb-4">
+		<div class="d-flex heading justify-content-center">
+			<h2 class="fw-bold">Winter</h2>
+		</div>
+</div>
+
+<div class="row mb-4">
+	<div class=" col-xl-3 col-lg-3 col-md-6 col-sm-12 ">
+		<a href=""><img src="{{asset('images/pic1.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic3.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic4.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic5.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+</div>
+<div class="row mb-5">
+	<div class="d-flex justify-content-center"><a class="btn bg-clr rounded-pill px-4 py-2 fs-6" href="">View More</a></div>
+</div>
+
+<div class="row mb-4">
+		<div class="d-flex heading justify-content-center">
+			<h2 class="fw-bold">Ladies</h2>
+		</div>
+	</div>
+<div class="row mb-4">
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic2.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic3.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic4.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic5.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+</div>
+<div class="row mb-5">
+	<div class="d-flex justify-content-center"><a class="btn bg-clr rounded-pill px-4 py-2 fs-6" href="">View More</a></div>
+</div>
+
+<div class="row mb-4">
+		<div class="d-flex heading justify-content-center">
+			<h2 class="fw-bold">Gents</h2>
+		</div>
+	</div>
+<div class="row mb-4">
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic1.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic2.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic3.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic4.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+</div>
+<div class="row mb-5">
+	<div class="d-flex justify-content-center"><a class="btn bg-clr rounded-pill px-4 py-2 fs-6" href="">View More</a></div>
+</div>
+
+<div class="row mb-4">
+		<div class="d-flex heading justify-content-center">
+			<h2 class="fw-bold">Bride</h2>
+		</div>
+	</div>
+<div class="row mb-4">
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic1.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic2.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic3.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic4.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+</div>
+
+<div class="row mb-5">
+	<div class="d-flex justify-content-center"><a class="btn bg-clr rounded-pill px-4 py-2 fs-6" href="">View More</a></div>
+</div>
+
+<div class="row mb-4">
+		<div class="d-flex heading justify-content-center">
+			<h2 class="fw-bold">Groom</h2>
+		</div>
+	</div>
+<div class="row mb-4">
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic1.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic2.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic3.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic4.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+</div>
+
+<div class="row mb-5">
+	<div class="d-flex justify-content-center"><a class="btn bg-clr rounded-pill px-4 py-2 fs-6" href="">View More</a></div>
+</div>
+
+<div class="row mb-4">
+		<div class="d-flex heading justify-content-center">
+			<h2 class="fw-bold">Kids</h2>
+		</div>
+	</div>
+<div class="row mb-4">
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic1.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic2.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic3.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic4.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+</div>
+
+<div class="row mb-5">
+	<div class="d-flex justify-content-center"><a class="btn bg-clr rounded-pill px-4 py-2 fs-6" href="">View More</a></div>
+</div>
+
+<div class="row mb-4">
+		<div class="d-flex heading justify-content-center">
+			<h2 class="fw-bold">Party Wear</h2>
+		</div>
+	</div>
+<div class="row mb-4">
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic1.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic2.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic3.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+	<div class="col-xl-3 col-lg-3 col-md-6 col-sm-12">
+		<a href=""><img src="{{asset('images/pic4.jpg')}}" class="img-thumbnail" alt="..."></a>
+	</div>
+</div>
+
+<div class="row mb-5">
+	<div class="d-flex justify-content-center"><a class="btn bg-clr rounded-pill px-4 py-2 fs-6" href="">View More</a></div>
+</div>
+
+</div>
+	</div>
+
+	<div class="container-fluid banner-section mt-3 ">
+		
+	</div>
+	<div class="container">
+		<div class="row justify-content-center mb-4">
+			<div class="col-xl-6 col-lg-6 col-md-6 col-sm-6 px-4 py-4 justify-content-center bg-clr border border-light">
+				<h3 class="text-white text-center">Emporium Store</h3>
+				<p class="text-white pb-4 border-bottom text-center">16M Abdul Haque Rd, Trade Centre Commercial Area Phase 2 Johar Town, Lahore, Punjab 54000</p>
+				<p class="text-white text-center fs-5 fw-bold">Opening Hours</p>
+				<p class="text-white text-center">Monday to Sunday : 10AM to 11PM</p>
+			</div>
+		</div>
+	</div>
+    @include('Components.footer')
