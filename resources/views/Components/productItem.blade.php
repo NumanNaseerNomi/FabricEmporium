414ebbc4a3e5520a354d4e8c5e1a37462db22d17
AnoodSabir <anoodsabir12@gmail.com>
2022-07-01 00:21:35 -0700

    Create productItem.blade.php

---
+++ resources/views/Components/productItem.blade.php
@@ -0,0 +1,15 @@
+<div class="col-lg-3 col-md-6 col-sm-12  mb-4">
+    <div class="card" role="button" data-bs-toggle="modal" data-bs-target="#exampleModal" style="width: 18rem;">
+        <img src="https://i.pinimg.com/236x/df/ed/00/dfed0051cf3051df61dfd545a50e02b2.jpg" class="card-img-top" alt="...">
+        <div class="card-body text-center">
+            <h5 class="card-title ">Card title</h5>
+            <p class="card-text">
+                <span class="text-decoration-line-through text-danger">PKR 15000</span>
+                <span>PKR 10000</span>
+            </p>
+            <div class="d-grid gap-2">
+                <button class="btn btn-outline-primary" type="button">Show Detail</button>
+            </div>
+        </div>
+    </div>
+</div>
