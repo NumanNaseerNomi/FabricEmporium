bb88dc1e3cb4f58634764a905519cb2fa7901445
AnoodSabir <anoodsabir12@gmail.com>
2022-07-01 00:10:03 -0700

    Update HomeController.php

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,8 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    function index()
+    function home()
     {
-        return view("index");
+        return view("home");
     }
 }
