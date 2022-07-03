397a69a43df36e0f0eb8589666decf72cc303392
AnoodSabir <anoodsabir12@gmail.com>
2022-07-03 04:17:17 -0700

    Update productItem.blade.php

--- resources/views/Components/productItem.blade.php
+++ resources/views/Components/productItem.blade.php
@@ -1,7 +1,15 @@
 <div class="col-lg-3 col-md-6 col-sm-12  mb-4">
-    <div class="card" role="button" data-bs-toggle="modal" data-bs-target="#exampleModal" style="width: 18rem;">
+
+    <div class="card  position-relative " role="button" data-bs-toggle="modal" data-bs-target="#exampleModal" style="width: 18rem;">
+    
         <img src="https://i.pinimg.com/236x/df/ed/00/dfed0051cf3051df61dfd545a50e02b2.jpg" class="card-img-top" alt="...">
-        <div class="card-body text-center">
+        <div class="card-body text-center bg-white">
+        <div class=" bg-danger text-white fw-bold position-absolute top-0 start-0 p-1 m-1 rounded">
+  20% OFF 
+</div>
+<button type="button" class="btn btn-primary position-absolute top-0 end-0 m-1">
+<i class="far fa-heart" ></i>
+</button>
             <h5 class="card-title ">Card title</h5>
             <p class="card-text">
                 <span class="text-decoration-line-through text-danger">PKR 15000</span>
