e5dd5cc922a787c1405fe6acaf9ee156c3d58e66
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-07-03 21:32:17 +0500

    auth

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\UsersModel;
+
+class AuthController extends Controller
+{
+    function authView()
+    {
+        return view("authenticate");
+    }
+
+    function loginAuth(Request $request)
+    {
+        $request->validate(
+            [
+                '_token' => 'required',
+                'email' => 'required',
+                'password' => 'required',
+            ]
+        );
+
+        $user = UsersModel::where('email', $request->email)->first();
+
+        if($user && Hash::check($request->password, $user->password))
+        {
+            $request->session()->put('user', $user);
+            return redirect('/');
+        }
+        else
+        {
+            return back()->withInput();
+        }
+    }
+
+    function registerAuth(Request $request)
+    {
+        return $request;
+    }
+
+    function logout(Request $request)
+    {
+        $request->session()->flush();
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,12 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ShowMoreController;
-use App\Http\Controllers\SignAuthController;
-use App\Http\Controllers\CartController;
+// use App\Http\Controllers\SignAuthController;
+// use App\Http\Controllers\CartController;
+use App\Http\Controllers\AuthController;
 
+
+use Illuminate\Http\Request;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -22,7 +25,7 @@ Route::get('/', [HomeController::class, "home"]);
 Route::get('/myProfile', [ProfileController::class, "viewProfile"]);
 
 Route::get('/showMore', [ShowMoreController::class, "ShowMore"]);
-Route::get('/signup', [SignAuthController::class, "showSignup"]);
+// Route::get('/signup', [SignAuthController::class, "showSignup"]);
 //Route::get('/show/cart', [CartController::class, "viewCart"]);
 Route::get('/place-order', function () {
     return view('placeorder');
@@ -36,3 +39,24 @@ Route::get('/myBag', function () {
 Route::post('/addToCart', function (Request $request) {
     return $request;
 });
+
+Route::get('/signup', function () {
+    return view('signUp');
+});
+
+Route::get('/login', [AuthController::class, "authView"]);
+Route::post('/login', [AuthController::class, "loginAuth"]);
+
+Route::post('/register', [AuthController::class, "registerAuth"]);
+Route::get('/logout', [AuthController::class, "logout"]);
+// Route::get('/login', function () {
+//     return view('authenticate');
+// });
+
+// Route::post('/login', function (Request $request) {
+//     return $request;
+// });
+
+// Route::post('/register', function (Request $request) {
+//     return $request;
+// });
