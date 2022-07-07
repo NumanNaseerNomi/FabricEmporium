f6574496cf6fdbf27cfd543808eb903ad674c19f
AnoodSabir <anoodsabir12@gmail.com>
2022-07-07 00:50:28 -0700

    Create updateprofile.blade.php

---
+++ resources/views/updateprofile.blade.php
@@ -0,0 +1,82 @@
+@include('Components.header')
+<div class="container mb-5">
+    <!-- <nav>
+        <div class="nav nav-tabs nav-pills justify-content-center mb-5" id="nav-tab" role="tablist">
+            <button class="nav-link active" id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Login</button>
+            <button class="nav-link" id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Create Account</button>
+        </div>
+    </nav> -->
+    <div class="tab-content" id="nav-tabContent">
+        <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab" tabindex="0">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-4 border shadow px-3 py-4">
+                    <h2 class=" d-flex justify-content-center">Edit Profile</h2>
+                    <form action="{{ url('/login') }}" method="post" class="row g-3">
+                        @csrf
+                        <!-- <div class="col-md-12">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" name="email" class="form-control" id="email" required>
+                        </div> -->
+                        <div class="col-md-12">
+                            <label for="password" class="form-label">Old Password</label>
+                            <input type="password" name="password" class="form-control" id="password" required>
+                        </div> 
+                        <div class="col-md-12">
+                            <label for="password" class="form-label">New Password</label>
+                            <input type="password" name="password" class="form-control" id="password" required>
+                        </div>
+                        <div class="col-md-12">
+                            <label for="password" class="form-label">Confirm Password</label>
+                            <input type="password" name="password" class="form-control" id="password" required>
+                        </div>
+                        <div class="col-12">
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn btn-primary">Update</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab" tabindex="0">
+            <div class="row justify-content-center">
+                <div class="col-12 col-md-4 border shadow px-3 py-4 mb-3">
+                    <h3 class ="d-flex justify-content-center mb-3">Create New Account</h3>
+                    <form action="{{ url('/register') }}" method="post" class="row g-3">
+                        @csrf
+                        <div class="col-md-6">
+                            <label for="name" class="form-label">Name</label>
+                            <input type="text" name="name" class="form-control" id="name" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="email" class="form-label">Email</label>
+                            <input type="email" name="email" class="form-control" id="email" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="contactNumber" class="form-label">Contact Number</label>
+                            <input type="number" name="contactNumber" class="form-control" id="contactNumber" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="address" class="form-label">Address</label>
+                            <input type="text" name="address" class="form-control" id="address" required>
+                        </div>
+                        <div class="col-md-6">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" name="password" class="form-control" id="password" required>
+                        </div> 
+                        <div class="col-md-6">
+                            <label for="password_confirmation" class="form-label">Confirm Password</label>
+                            <input type="password" name="password_confirmation" class="form-control" id="password_confirmation" required>
+                        </div>
+                        <div class="col-12  mb-3">
+                            <div class="d-grid gap-2">
+                                <button type="submit" class="btn btn-primary">Register</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div> -->
+    </div>
+</div>
+@include('Components.footer')
