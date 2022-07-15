4798953956c26679903567ae37709aee7fc50630
NOMi <61705661+NumanNaseerNomi@users.noreply.github.com>
2022-07-16 02:54:54 +0500

    Update showMore.blade.php

--- resources/views/showMore.blade.php
+++ resources/views/showMore.blade.php
@@ -1,15 +1,4 @@
 @include('Components.header')
-@include('Components.navbar')
-<!-- <div class="container">
-    <div class="row mb-4">
-        <div class="col-xl-2 col-lg-2 col-md-3 col-sm-4"><a href=""><img class="brand-logo" src="{{ asset('images/logos/brandLogoBrown.png')}}"></a></div>
-        <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4"><a href=""><img class="brand-logo" src="{{ asset('images/logos/brandLogoBrown.png')}}"></a></div>
-        <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4"><a href=""><img class="brand-logo" src="{{ asset('images/logos/brandLogoBrown.png')}}"></a></div>
-        <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4"><a href=""><img class="brand-logo" src="{{ asset('images/logos/brandLogoBrown.png')}}"></a></div>
-        <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4"><a href=""><img class="brand-logo" src="{{ asset('images/logos/brandLogoBrown.png')}}"></a></div>
-        <div class="col-xl-2 col-lg-2 col-md-4 col-sm-4"><a href=""><img class="brand-logo" src="{{ asset('images/logos/brandLogoBrown.png')}}"></a></div>
-    </div>
-</div> -->
 <div class="container">
     <div class="row mt-4 mb-4">
         <h2 class="page-title d-flex justify-content-center">{{ $products[0]->category }}</h2>
