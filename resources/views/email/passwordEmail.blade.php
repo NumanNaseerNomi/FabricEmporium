73d90ee8732385386d2d219c08df6a71613a834d
AnoodSabir <anoodsabir12@gmail.com>
2022-07-16 20:14:29 -0700

    Create passwordEmail.blade.php

---
+++ resources/views/email/passwordEmail.blade.php
@@ -0,0 +1,5 @@
+<img  src="{{ asset('images/logos/brandLogoBrown.png')}}" width="180px">
+<h3>Hi {{ $user->name }}!</h3>
+<p>Wellcome to Fabric Emporium.</p>
+<p>To reset your password please enter the given OTP.</p>
+<p><strong>Your OTP is: </strong>{{ $otp }}</p>
